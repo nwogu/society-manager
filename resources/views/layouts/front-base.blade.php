9c542d05f298658202027ed752876e81542a6276
Gabriel Nwogu <nwogugabriel@gmail.com>
2019-03-21 21:40:26 +0100

    small tweak

--- resources/views/layouts/front-base.blade.php
+++ resources/views/layouts/front-base.blade.php
@@ -80,7 +80,7 @@
       <div class="row align-items-center justify-content-xl-between">
         <div class="col-xl-6">
           <div class="copyright text-center text-xl-left text-muted">
-            &copy; 2018 <a href="{{ route('home') }}" class="font-weight-bold ml-1" target="_blank">Society Manager</a>
+            &copy; {{date('Y')}} <a href="{{ route('home') }}" class="font-weight-bold ml-1" target="_blank">Society Manager</a>
           </div>
         </div>
       </div>
