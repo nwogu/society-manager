1bbb69745328cb4a892f6c3b8f489ca3a998a85c
Nwogu <nwogugabriel@gmail.com>
2020-05-15 22:56:51 +0000

    fix provider error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceRootUrl(\Config::get('app.url'));
+        \URL::forceRootUrl(config('app.url'));
 
-        if (\Str::contains(\Config::get('app.url'), 'https://')) {
+        if (str_contains(config('app.url'), 'https://')) {
             \URL::forceScheme('https');
         }
 
